29f0af5a74daffb5449e83f19aa96e75f43178ba
ZizahHz <20ftt1812@student.pb.edu.bn>
2022-04-19 16:18:52 +0800

    added css at index.php

--- index.php
+++ index.php
@@ -4,53 +4,11 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="style.css" />
     <title>Login</title>
 </head>
 
-<style>
-.login h1, .login h2 {
-    text-align: center;
-}
-
-.login input {
-   width: 100%;
-}
-
-.welcome .player-name {
-    margin: 30px 0;
-}
-
-button {
-    background-color: #09aeae;
-    color: #FFF;
-    border: 0;
-    border-radius: 10px;
-    padding: 1em 2em;
-    cursor: pointer;
-}
-
-button[disabled] {
-    cursor: not-allowed;
-    opacity: .5;
-}
-
-body, input, button {
-    font-family: 'Roboto', Arial, Helvetica, sans-serif;
-    font-size: 12pt;
-    color: #333;
-    text-align: center;
-}
-
-input[type=text] {
-    border-radius: 10px;
-    border: 1px solid black;
-    margin: 3px 0;
-    width: 265px;
-    padding: 8px 5px;
-}
-
-</style>
-<body>
+<body class="bkgrdindex">
 
 <form class="background" action="question.php" method="post">
 <div class="login">
