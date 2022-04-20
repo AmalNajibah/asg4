bc0dc1c14ce54bf8e4a838abbcbb72842473a8ee
ZizahHz <20ftt1812@student.pb.edu.bn>
2022-04-20 23:12:32 +0800

    added hover css at index.php for biginner and advance button

--- index.php
+++ index.php
@@ -4,9 +4,105 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" type="text/css" href="style.css" />
+    <!-- <link rel="stylesheet" type="text/css" href="style.css" /> -->
     <title>Login</title>
 </head>
+
+<style>
+body, html {
+  height: 100%;
+  margin: 0;
+}
+
+.bkgrdindex{
+    background-image: url('img/index1.jpg');
+    background-repeat:no-repeat;
+    background-size:cover;
+    /* border: 5px solid white; */
+    height:100%
+
+}
+    
+.bkgrdindex, input, button {
+   font-family: 'Roboto', Arial, Helvetica, sans-serif;
+   font-size: 12pt;
+   color: #333;
+   text-align: center;
+}
+
+button {
+   background-color: #09aeae;
+   color: #FFF;
+   border: 0;
+   border-radius: 10px;
+   padding: 1em 2em;
+   cursor: pointer;
+}
+
+button[disabled] {
+   cursor: not-allowed;
+   opacity: .5;
+}
+
+//hover
+button {
+  overflow: hidden;
+  transition: all 300ms ease;
+}
+
+/* button h4 {
+  color: #111;
+} */
+
+button:after {
+  width: 300%;
+  height: 300%;
+  left: -300%;
+  transform: rotate(45deg);
+  background-color: #f21170;
+  transition: all 300ms ease;
+}
+
+button:hover {
+  color: #fff;
+  z-index: 10;
+  font-weight: 600;
+}
+
+button:hover::after {
+  left: -100%;
+}
+
+button:hover {
+  border: 2px solid #f21170;
+}
+
+
+
+
+
+.login h1, .login h2 {
+   text-align: center;
+}
+
+.login input {
+  width: 100%;
+}
+
+.welcome .player-name {
+   margin: 30px 0;
+}
+
+input[type=text] {
+   border-radius: 10px;
+   border: 1px solid black;
+   margin: 3px 0;
+   width: 265px;
+   padding: 8px 5px;
+}
+
+
+</style>
 
 <body class="bkgrdindex">
 
